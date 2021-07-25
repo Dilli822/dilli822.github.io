c637a79cb5a416549fa37f59e2daa97d9ba28edd
DILLI  HANG  RAI <dillihangrae@gmail.com>
2021-07-25 13:38:13 +0545

    [add] adds style and html were added with nav-bar features

---
+++ script/index.js
@@ -0,0 +1,12 @@
+
+
+    function openNav(){
+        document.getElementsById('myNav').style.width = "100%";
+
+    }
+
+    // closing nav bar btn
+    function closeNav(){
+        document.getElementsById('myNav').style.width = "0%";
+    }
+    
