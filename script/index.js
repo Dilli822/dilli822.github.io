d500845e5014ffc1aa7829873fb75b7cae5b6f28
dilli <dillihangrae@gmail.com>
2021-07-26 12:55:45 +0545

    css and html changes

--- script/index.js
+++ script/index.js
@@ -2,47 +2,31 @@
 
    
         // JS script 
-     
-            // clearing and reloading the console
-            console.clear();
-            console.log('JS Relaoded')
+
         fetch("https://api.github.com/users/dilli822")
         .then(response => response.json())
         .then(function(data){
             console.log(data);
 
-            let public_reposNum = data['public_repos']
-            let public_reposInformation= `Currently I have ${public_reposNum} repos on github.`
-            
-            document.getElementById('avatar').src = data["avatar_url"]
             document.getElementById('fullName').textContent = data["name"]
-            document.getElementById('bio').textContent  =  data["bio"]
-            document.getElementById('location').textContent  =  data["location"]
-
-            document.getElementById('public_reposNumber').textContent = public_reposInformation
-            document.getElementById('githubLink').href= data ['url']
-            document.getElementById('htmlLink').href= data ['html_url']
-            document.getElementById('reposLink').href= data['repos_url']
-
-
-          
+            document.getElementById('bio').textContent = data["bio"]
         })
 
-        fetch("https://api.github.com/users/Dilli822/repos")
-        .then(response => response.json())
-        .then (function(data2){
-            console.log(data2)
-           console.log(data2[1])
-           console.log(data2[4])
-           console.log(data2[8])
-           console.log(data2[12])
-           console.log(data2[17])
+        // fetch("https://api.github.com/users/Dilli822/repos")
+        // .then(response => response.json())
+        // .then (function(data2){
+        //     console.log(data2)
+        //    console.log(data2[1])
+        //    console.log(data2[4])
+        //    console.log(data2[8])
+        //    console.log(data2[12])
+        //    console.log(data2[17])
 
-             //this will unhide the main element after hard reset
-            document.getElementById('main-container').hidden= false
+        //      //this will unhide the main element after hard reset
+        //     document.getElementById('main-container').hidden= false
 
-            //remove the loading after the main element is unhidden
-            document.getElementById('loading').hidden= true
-        })   
+        //     //remove the loading after the main element is unhidden
+        //     document.getElementById('loading').hidden= true
+        // })   
 
     
