12065c17842fe0b3f33337882cb32dc2965290ad
dilli <dillihangrae@gmail.com>
2023-10-17 20:10:42 +0545

    handled the mobile screen. with the proper message. removed the console.log message

--- resources/script/csit/2ndSem/PastQuestionSolution/fullScreenButtons.js
+++ resources/script/csit/2ndSem/PastQuestionSolution/fullScreenButtons.js
@@ -86,3 +86,13 @@
          const loader = document.getElementById('loaders');
          loader.style.display = 'none';
        }
+
+
+       function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+      }
+      
+      function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+      }
+      
